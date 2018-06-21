deb05c8dfe55659cfe3ee2a8ef8d106183457fea
Leslie Alldridge <leslie.alldridge@gmail.com>
2018-06-21 21:06:51 +1200

    created basic todolist with react
    created basic todo list functionality , further refinement still to come

---
+++ TodoList.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import TodoItems from "./TodoItems";
+import "./TodoList.css";
+
+class TodoList extends Component {
+    constructor(props){
+        
+        super(props);
+        //sets current state to an array
+        this.state = {
+            items: []
+        };
+
+        this.addItem = this.addItem.bind(this);
+    }
+    //add item method , create item 
+    addItem(e){
+        if (this._inputElement.value !== ""){
+            var newItem = {
+                text: this._inputElement.value,
+                key: Date.now()
+            };
+            // set state to new item and this creates a new array each time
+            this.setState((prevState) => {
+                return {
+                    items: prevState.items.concat(newItem)
+                };
+            });
+        
+        }
+        this._inputElement.value = "";
+        console.log(this.state.items);
+        //this stops the page from reloading on form submit
+        e.preventDefault();
+    }
+    
+
+    render() {
+        return (
+            <div className="todoListMain">
+            <div className="header">
+            <form onSubmit={this.addItem}>
+                <input ref={(a) => this._inputElement = a}
+                        placeholder="enter task">
+                </input>
+                <button type="submit">add</button>
+            </form>
+            </div>
+            <TodoItems entries={this.state.items}/>
+            </div>
+        );
+    }
+}
+
+export default TodoList;
